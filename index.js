a19779a1505db243237f53429414d6797fcccd5e
fuzious <arpitsrivastava2012@gmail.com>
2020-10-02 04:13:07 +0530

    major contract interaction

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const {CourseCreatorAddress,CourseCreatorAbi,CourseAbi,CourseCreatorBytecode} = require('./config')
 
 var web3 = new Web3("")
-var faucetPrivateKey = "97d8cb40d55f97fa4a9dcbb9d89b159128b95043edfd835467553f3b5c69d7af"
-// var faucetPublicKey = "0x57Ac4E60a3fDaDec7e6b51b28488B392447801F4"
+var faucetPublicKey ;
+
+
 
 async function newAccount() {
     var newAccount = web3.eth.accounts.create()
     console.log(JSON.stringify(newAccount))
-    const provider = new HDWalletProvider(faucetPrivateKey,"https://rpc-mumbai.matic.today/")
-    web3=new Web3(provider)
-    console.log('new Account'+JSON.stringify(web3.eth.accounts.privateKeyToAccount(faucetPrivateKey)))
-    // web3.eth.sendTransaction({to:newAccount.address, from:faucetPublicKey, value:web3.utils.toWei("0.1", "ether")}).then(function(receipt){
-    //     console.log(receipt)
-    // })
+    web3=new Web3("http://localhost:8545")
+
+    faucetPublicKey = await web3.eth.getAccounts(console.log)
+
     web3.eth.sendTransaction({
-        from: web3.eth.accounts.privateKeyToAccount(faucetPrivateKey).address,
+        from: await faucetPublicKey[0],
         to: newAccount.address,
         value: '100000000000000000'
     },function(error, hash){
@@ -25,12 +24,12 @@ async function newAccount() {
         console.log(receipt)
     });
 
-    console.log({publicKey: newAccount.address,privateKey: newAccount.privateKey.substring(2)})
-    return {publicKey: newAccount.address,privateKey: newAccount.privateKey.substring(2)}
+    console.log({"publicKey": newAccount.address,"privateKey": newAccount.privateKey.substring(2)})
+    return {"publicKey": newAccount.address,"privateKey": newAccount.privateKey.substring(2)}
 }
 
 async function submit (privateKey, courseAddress, filehash) {
-    const provider = new HDWalletProvider(privateKey,"https://rpc-mumbai.matic.today/")
+    const provider = new HDWalletProvider(privateKey,"http://localhost:8545")
     web3=new Web3(provider)
     const accounts = await web3.eth.getAccounts()
 
@@ -39,20 +38,20 @@ async function submit (privateKey, courseAddress, filehash) {
     return "true"
 }
 
-async function deployContract (privateKey) {
+async function deployContract () {
 
-    const provider = new HDWalletProvider(privateKey,"https://rpc-mumbai.matic.today/")
-    web3=new Web3(provider)
+    web3=new Web3("http://localhost:8545")
+
+    faucetPublicKey = await web3.eth.getAccounts(console.log)
 
     let deploy_contract = new web3.eth.Contract(CourseCreatorAbi);
-    let account = web3.eth.accounts.privateKeyToAccount(faucetPrivateKey).address;
     let payload = {
         data : CourseCreatorBytecode
     }
     let parameter = {
-        from: account,
+        from: await faucetPublicKey[0],
         gas: web3.utils.toHex(800000),
-        gasPrice: web3.utils.toHex(web3.utils.toWei('30', 'gwei'))
+        gasPrice: web3.utils.toHex(web3.utils.toWei('1', 'gwei'))
     }
     deploy_contract.deploy(payload).send(parameter, (err, transactionHash) => {
         console.log('Transaction Hash :', transactionHash);
@@ -61,6 +60,8 @@ async function deployContract (privateKey) {
     })
 }
 
-// deployContract("97d8cb40d55f97fa4a9dcbb9d89b159128b95043edfd835467553f3b5c69d7af")
-newAccount()
+// deployContract()
+// newAccount()
 // submit("97d8cb40d55f97fa4a9dcbb9d89b159128b95043edfd835467553f3b5c69d7af","0x8FACefb6d5Ca1F2d7bCF88FbaAe1093bae733895","hi4")
+
+// ethermintcli keys unsafe-export-eth-key mykey 
