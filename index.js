fa582ce635bc5799bc2b462d95848269404270ee
fuzious <arpitsrivastava2012@gmail.com>
2020-10-02 02:17:27 +0530

    public key removed

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const {CourseCreatorAddress,CourseCreatorAbi,CourseAbi,CourseCreatorBytecode} = 
 
 var web3 = new Web3("")
 var faucetPrivateKey = "97d8cb40d55f97fa4a9dcbb9d89b159128b95043edfd835467553f3b5c69d7af"
-var faucetPublicKey = "0x57Ac4E60a3fDaDec7e6b51b28488B392447801F4"
+// var faucetPublicKey = "0x57Ac4E60a3fDaDec7e6b51b28488B392447801F4"
 
 async function newAccount() {
     var newAccount = web3.eth.accounts.create()
     console.log(JSON.stringify(newAccount))
     const provider = new HDWalletProvider(faucetPrivateKey,"https://rpc-mumbai.matic.today/")
     web3=new Web3(provider)
-
+    console.log('new Account'+JSON.stringify(web3.eth.accounts.privateKeyToAccount(faucetPrivateKey)))
     // web3.eth.sendTransaction({to:newAccount.address, from:faucetPublicKey, value:web3.utils.toWei("0.1", "ether")}).then(function(receipt){
     //     console.log(receipt)
     // })
     web3.eth.sendTransaction({
-        from: faucetPublicKey,
+        from: web3.eth.accounts.privateKeyToAccount(faucetPrivateKey).address,
         to: newAccount.address,
         value: '100000000000000000'
     },function(error, hash){
@@ -45,7 +45,7 @@ async function deployContract (privateKey) {
     web3=new Web3(provider)
 
     let deploy_contract = new web3.eth.Contract(CourseCreatorAbi);
-    let account = faucetPublicKey;
+    let account = web3.eth.accounts.privateKeyToAccount(faucetPrivateKey).address;
     let payload = {
         data : CourseCreatorBytecode
     }
@@ -62,5 +62,5 @@ async function deployContract (privateKey) {
 }
 
 // deployContract("97d8cb40d55f97fa4a9dcbb9d89b159128b95043edfd835467553f3b5c69d7af")
-// newAccount()
+newAccount()
 // submit("97d8cb40d55f97fa4a9dcbb9d89b159128b95043edfd835467553f3b5c69d7af","0x8FACefb6d5Ca1F2d7bCF88FbaAe1093bae733895","hi4")
