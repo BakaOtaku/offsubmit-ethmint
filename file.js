47624f0630efc0f7f476e544f0ea43d3234fdb07
Aman Raj <archanaamanraj@gmail.com>
2020-10-03 02:43:09 +0530

    decrypt -> encrypt

--- file.js
+++ file.js
@@ -2,28 +2,26 @@ const crypto = require("crypto");
 // const Algorithm = "aes-128-ecb";
 const fs = require("fs");
 
-function Decrypt_AES() {
+function Encrypt_AES() {
 
     const ALGORITHM = 'aes-128-ecb';
-    const key = "7552383524831970";
-    const ENCRYPTION_KEY = key;
-
+    const ENCRYPTION_KEY = "7552383524831970";
     var IV = Buffer.alloc(0);
-    var decipher = crypto.createDecipheriv(ALGORITHM, ENCRYPTION_KEY, IV);
-    decipher.setAutoPadding(false);
-    var input = fs.createReadStream('Qmd6cVGCXBEJPwN2xx9VpNjtU4M12Z9LaQkPKCgyoRHPJT');
-    var output = fs.createWriteStream('60178910.pdf');
 
-    input.pipe(decipher).pipe(output);
+    var cipher = crypto.createCipheriv(ALGORITHM, ENCRYPTION_KEY, IV);
+    var input = fs.createReadStream('Aman_Raj.pdf');
+    var output = fs.createWriteStream('encrypted.pdf.enc');
+
+    input.pipe(cipher).pipe(output);
 
     output.on('finish', function () {
-        console.log('Decrypted file written to disk!');
+        console.log('Encrypted file written to disk!');
     });
     output.on('error', function (e) {
         console.log(e);
      });
 }
-Decrypt_AES()
+Encrypt_AES()
 
 
 // sample file used : https://ipfs.io/ipfs/Qmd6cVGCXBEJPwN2xx9VpNjtU4M12Z9LaQkPKCgyoRHPJT
