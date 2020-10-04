2337e6a4b4e65044c4bbd5d3a28fe42e6b910cb5
fuzious <arpitsrivastava2012@gmail.com>
2020-10-04 11:03:54 +0530

    testing local

--- file.js
+++ file.js
@@ -11,8 +11,8 @@ function Decrypt_AES() {
     var IV = Buffer.alloc(0);
     var decipher = crypto.createDecipheriv(ALGORITHM, ENCRYPTION_KEY, IV);
     decipher.setAutoPadding(false);
-    var input = fs.createReadStream('Qmd6cVGCXBEJPwN2xx9VpNjtU4M12Z9LaQkPKCgyoRHPJT');
-    var output = fs.createWriteStream('60178910.pdf');
+    var input = fs.createReadStream('Encrypted.enc');
+    var output = fs.createWriteStream('Decrypted.pdf');
 
     input.pipe(decipher).pipe(output);
 
@@ -23,7 +23,33 @@ function Decrypt_AES() {
         console.log(e);
      });
 }
-Decrypt_AES()
+
+function Encrypt_AES() {
+
+    const ALGORITHM = 'aes-128-ecb';
+    const key = "7552383524831970";
+    const ENCRYPTION_KEY = key;
+
+    var IV = Buffer.alloc(0);
+    var cipher = crypto.createCipheriv(ALGORITHM, ENCRYPTION_KEY, IV);
+    // cipher.setAutoPadding(false);
+    var input = fs.createReadStream('60178910.pdf');
+    var output = fs.createWriteStream('Encrypted.enc');
+
+    input.pipe(cipher).pipe(output);
+
+    output.on('finish', function () {
+        console.log('Enrypted file written to disk!');
+     Decrypt_AES()
+
+    });
+    output.on('error', function (e) {
+        console.log(e);
+     });
+    
+
+}
+Encrypt_AES()
 
 
 // sample file used : https://ipfs.io/ipfs/Qmd6cVGCXBEJPwN2xx9VpNjtU4M12Z9LaQkPKCgyoRHPJT
